ab50fcd5b64e3d0e96df0b444ba107a7267e16d7
Emi00093522-cloud <00093522@uca.edu.sv>
2025-10-30 23:14:57 -0600

    Refactor app.py to remove DB connection logic
    Removed database connection code and called mostrar_venta function.

--- app.py
+++ app.py
@@ -1,33 +1,11 @@
-import mysql.connector
-import os
+# app.py
 import streamlit as st
+from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 
-# Obtener las credenciales de las variables de entorno (seguridad)
-host = os.getenv("DB_HOST", "beu5uv04siiubuvtnhzp-mysql.services.clever-cloud.com")
-user = os.getenv("DB_USER", "usqhvy7ik0yfoilx")
-password = os.getenv("DB_PASSWORD", "ghEpZtJkPEsx8yL9WiDa")
-database = os.getenv("DB_NAME", "beu5uv04siiubuvtnhzp")
+# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+mostrar_venta()
 
-# Intentar establecer la conexión
-try:
-    conn = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    
-    # Verificar si la conexión fue exitosa
-    if conn.is_connected():
-        st.success("Conexión exitosa a la base de datos MySQL.")
-    else:
-        st.error("No se pudo conectar a la base de datos.")
-        
-except mysql.connector.Error as err:
-    # Mostrar el error si la conexión falla
-    st.error(f"Error al conectar con la base de datos: {err}")
 
-finally:
-    if conn.is_connected():
-        # Cerrar la conexión
-        conn.close()
+
+
+

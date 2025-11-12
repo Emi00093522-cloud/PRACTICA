e43f74b685850b56e2e852d54cd2524d521a4978
ek-mtz07 <00032022@uca.edu.sv>
2025-11-11 20:03:07 -0600

    Update and rename venta.py to bienvenida.py

--- modulos/venta.py
+++
@@ -1,6 +0,0 @@
-# modulos/venta.py
-import streamlit as st
-def mostrar_venta():
- # Aquí se muestra "Hola Mundo"
- st.title("Hola Mundo")
- st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
